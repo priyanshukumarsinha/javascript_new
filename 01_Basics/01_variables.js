ba3d72207c1fd8230fed34a2302e8d51fc27d38d
Priyanshu Kumar Sinha <79042642+priyanshukumarsinha@users.noreply.github.com>
2024-04-13 15:50:08 +0000

    01 Variables done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 14425
+let accountName = "Priyanshu"
+var accountEmail = "pk@gmail.com"
+accountState = "Bengaluru"
+
+// accountId = 2 //Not Allowed
+
+console.table([accountId, accountName, accountEmail, accountState])
