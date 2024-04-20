78dacf833d5c74e0f6c0c1a4d0c3252a09cc0c7b
Priyanshu Kumar Sinha <79042642+priyanshukumarsinha@users.noreply.github.com>
2024-04-20 21:37:40 +0000

    control flow done

---
+++ 04_control_flow/controlflow.js
@@ -0,0 +1,19 @@
+// if
+// if else
+// if - else if - else
+// switch - case
+// falsy values - false, 0, -0, "", null, undefined, NaN
+// truthy values
+
+// Nullish Coalescing Operator (??) null undefined
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 10
+// val1 = undefined ?? null ?? 10
+
+// console.log(val1)
+
+
+// Terniary Operator
+console.log(5>3?5:3)
